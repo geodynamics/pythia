42aadca8efdfffa27de6b1387d544276efd376f1
Leif Strand <leif@geodynamics.org>
2006-10-09 12:12:10 +0000

    Fix-up sys.path so that e.g., "import pyre" works.
    In the application, a single "import pythia" is needed
    somewhere (e.g., Foo/__init__.py, or the top-level script).
    If this were C++, "import pythia" would be equivalent to
    
        using namespace pythia;
    
    It would be nice if pkg_resources provided an API for
    this:
    
        from pkg_resources import use
        use('pythia', __name__)
    
    Or maybe a language extension? :-)
    
    The difference being that this would modify __path__
    instead of sys.path for modules.  At least, I think that
    would work.  I tried to tweak e.g., pyre.__path__ from
    within pythia, but it lead to an infinite regress no
    matter how I did it.  It might require a custom loader
    (Pmw has one), I'm not sure.

---
+++ pythia/__init__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#                      California Institute of Technology
+#                        (C) 2006  All Rights Reserved
+#
+# {LicenseText}
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+
+
+import sys
+sys.path.append(__path__[0])
+
+
+# end of file 
