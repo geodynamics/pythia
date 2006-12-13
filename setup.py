588c141f27aa71d21b727142a5650c7f272aed10
Leif Strand <leif@geodynamics.org>
2006-12-13 00:40:17 +0000

    Bumped version.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from merlin import setup, find_packages
 setup(
     
     name = 'pythia', 
-    version = '0.8.1.1',
+    version = '0.8.1.2',
 
     zip_safe = False,
     packages = find_packages(),
