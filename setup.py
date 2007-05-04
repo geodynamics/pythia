a3ae40bb23a2c5159a6f9e18ab346cc62b9bf7cf
Leif Strand <leif@geodynamics.org>
2007-05-04 01:02:49 +0000

    [pythia v0.8.1.3b6]
    Reverted r6610.  Issue105 is fixed by merlin v1.2.

--- setup.py
+++ setup.py
@@ -52,7 +52,4 @@ setup(
     license = 'BSD',
     url = 'http://www.geodynamics.org/cig/software/packages/cs/pythia/',
     
-    dependency_links = [
-    'http://www.geodynamics.org/cig/software/packages/cs/pythia/deps/',
-    ]
     )
