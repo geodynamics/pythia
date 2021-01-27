d7e62ceae14f2202626d25a780f768f1ab941c1e
Brad Aagaard <baagaard@usgs.gov>
2021-01-26 21:25:17 -0700

    Fix nemesis merge/rebase.

--- setup.py
+++ setup.py
@@ -2,21 +2,3 @@
 
 import setuptools
 setuptools.setup()
-=======
-
-from distutils.core import setup
-
-setup(
-    
-    name = 'nemesis', 
-    version = '1.1',
-
-    zip_safe = False,
-    
-    author = 'Leif Strand',
-    author_email = 'leif@geodynamics.org',
-    description = """A Python interpreter which embeds MPI.""",
-    license = 'BSD',
-    url = 'http://www.geodynamics.org/cig/software/pythia/',
-
-)
