e521353bd51098833cd5484b8a6673fe425dcebd
Brad Aagaard <baagaard@usgs.gov>
2022-04-01 20:32:45 -0600

    Improve Markdown output for pyre_doc_components.py

--- bin/pyre_doc_components.py
+++ bin/pyre_doc_components.py
@@ -45,20 +45,22 @@ class App():
                 filename = App._get_filename(output_path, module_path)
                 print(f"Working on {filename}...")
                 self._process_class(cls_name, filename)
-        self._write_toc(output_path)
+        package = args.package
+        self._write_toc(output_path, package)
 
-    def _write_toc(self, output_path):
+    def _write_toc(self, output_path, package):
         """Write table of contents.
 
         :param output_path: Output path for Markdown pages.
+        :param package: Name of package,
         """
         for key,items in self.toc.items():
-            filename = pathlib.Path(key) / "index.md"
+            filename = output_path / pathlib.Path(key) / "index.md"
             with open(filename, "w") as mfile:
                 self._write_subtoc(mfile, key, items)
         filename = output_path / "index.md"
         with open(filename, "w") as mfile:
-            self._write_maintoc(mfile, self.toc)
+            self._write_maintoc(mfile, self.toc, package)
         
     @staticmethod
     def _write_subtoc(mfile, key, items):
@@ -66,6 +68,7 @@ class App():
         """
         items = sorted(items)
         mfile.write(f"# {key}\n\n")
+        mfile.write("% WARNING: Do not edit; this is a generated file.\n\n")
         mfile.write(":::{toctree}\n")
         mfile.write("---\nmaxdepth: 1\n---\n")
         for item in items:
@@ -73,8 +76,9 @@ class App():
         mfile.write(":::\n")
 
     @staticmethod
-    def _write_maintoc(mfile, toc):
-        mfile.write("(sec-user-components)=\n# PyLith Components\n\n")
+    def _write_maintoc(mfile, toc, package):
+        mfile.write(f"(sec-user-components)=\n# {package} Components\n\n")
+        mfile.write("% WARNING: Do not edit; this is a generated file.\n\n")
         mfile.write(":::{toctree}\n")
         mfile.write("---\nmaxdepth: 2\n---\n")
         keys = sorted(toc.keys())
@@ -105,33 +109,61 @@ class App():
     @staticmethod
     def _write_markdown(mfile, cls_obj, cls_name, full_name):
         """Show documentation for class with name cls_name in module."""
-        mfile.write(f"# {cls_name}\n\n")
-        mfile.write(f"Full name: `{full_name}`\n\n")
+        def write_props(mfile, header, sout):
+            if not len(sout):
+                return
+            mfile.write(header)
+            mfile.write(sout)
+            mfile.write("\n")
+            
+        def write_config(mfile, cls_name, block_type, block):
+            mfile.write(":::{code-block} " + block_type + "\n")
+            mfile.write(block)
+            mfile.write(":::\n\n")
+            
+        def dedent(text):
+            lines = text.split("\n")[1:]
+            indent = len(lines[0]) - len(lines[0].lstrip())
+            lines_dedent = []
+            for line in lines:
+                lines_dedent.append(line[indent:])
+            return "\n".join(lines_dedent)
 
+        mfile.write(f"# {cls_name}\n\n")
+        mfile.write("% WARNING: Do not edit; this is a generated file!\n")
+        mfile.write(f"Full name: `{full_name}`\n\n")
         if cls_obj.__doc__:
-            mfile.write(cls_obj.__doc__)
-            mfile.write("\n\n")
+            mfile.write(dedent(cls_obj.__doc__))
+            mfile.write("\n")
         else:
             print(f"Component {cls_name} missing doc string.")
 
         if App._has_function(cls_obj, "showComponents"):
             with contextlib.redirect_stdout(io.StringIO()) as sout:
                 cls_obj.showComponents(formatter=MarkdownMyST)
-            mfile.write("## Pyre Facilities\n\n")
-            mfile.write(sout.getvalue())
-            mfile.write("\n")
+            write_props(mfile, "## Pyre Facilities\n\n", sout.getvalue())
         if App._has_function(cls_obj, "showProperties"):
             with contextlib.redirect_stdout(io.StringIO()) as sout:
                 cls_obj.showProperties(formatter=MarkdownMyST, omitHelp=True)
-            mfile.write("## Pyre Properties\n\n")
-            mfile.write(sout.getvalue())
-            mfile.write("\n")
+            write_props(mfile, "## Pyre Properties\n\n", sout.getvalue())
+        if hasattr(cls_obj, "DOC_CONFIG"):
+            docs = cls_obj.DOC_CONFIG
+            mfile.write("## Example\n\n")
+            mfile.write(f"Example of setting `{cls_name}` Pyre properties and facilities in a parameter file.\n\n")
+            if len(docs) == 1:
+                block_type, block = docs.popitem()
+                write_config(mfile, cls_name, block_type, dedent(block))
+            elif len(docs) > 1:
+                for block_type, block in docs.items():
+                    write_config(mfile, cls_name, block_type, dedent(block))
 
     def _parse_command_line(self):
         """Parse command line arguments.
         """
         description = "Generate Markdown documentation for Pyre components."
         parser = argparse.ArgumentParser(description=description)
+        parser.add_argument("--package", action="store", dest="package", metavar="NAME", type=str, required=True,
+            help="Name of package.")
         parser.add_argument("--src-path", action="store", dest="src_path", metavar="PATH", type=str, required=True,
             help="Path to search for Pyre components.")
         parser.add_argument("--output-path", action="store", dest="output_path", metavar="PATH", type=str, default=".",
